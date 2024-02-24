dfae78aaa20aa6c14a037ea6448887973efbc531
Jiuzhou Zhang <jz2018@hw.ac.uk>
2024-02-24 17:58:08 +0000

    Nothing

--- src/AirportSystem.java
+++ src/AirportSystem.java
@@ -5,27 +5,8 @@ import GUI.*;
 import static Algorithm.ReadLists.readFlightList;
 
 public class AirportSystem {
-    private PassengerList passengerList;
-    private FlightList flightList;
-    private FlightDetailsList flightDetailsList;
 
     public AirportSystem() {
-        passengerList = new PassengerList();
-        flightList = new FlightList();
-        readFlightList(flightList);
-        flightDetailsList = ReadLists.readPassengerList(passengerList, flightList);
-    }
-    public PassengerList getPassengerList() {
-        return passengerList;
-    }
-    public FlightList getFlightList() {
-        return flightList;
-    }
-    public void showGUI() {
-//        new ProgramGUI(passengerList, flightList, flightDetailsList).run();
-    }
-    public FlightDetailsList getFlightDetailsList() {
-        return flightDetailsList;
     }
 
     /**
@@ -33,8 +14,5 @@ public class AirportSystem {
      * @param args None.
      */
     public static void main(String[] args) {
-        AirportSystem airportSystem = new AirportSystem();
-        airportSystem.showGUI();
-        new SystemControlInterface(airportSystem.getFlightDetailsList(), airportSystem.getFlightList()).run();
-    }
+         }
 }
