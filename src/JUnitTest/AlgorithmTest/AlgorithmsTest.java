210e7ece3c181a1f76cd84d5002d33cce33e564e
Jiuzhou Zhang <jz2018@hw.ac.uk>
2024-03-15 21:43:05 +0000

    Rebase

--- src/JUnitTest/AlgorithmTest/AlgorithmsTest.java
+++ src/JUnitTest/AlgorithmTest/AlgorithmsTest.java
@@ -1,23 +1,13 @@
 package JUnitTest.AlgorithmTest;
 
-import model.Algorithm.*;
-import model.DataStructure.*;
-
-//JUnit 4
-//import org.junit.Test;
-//import static org.junit.Assert.assertEquals;
-
-//JUnit 5, including
-//Test: This annotation denotes that a method is a test method.
-//@BeforeEach: This annotation denotes that the annotated method will be executed before each test method.
-//@AfterEach: This annotation denotes that the annotated method will be executed after each test method.
-//@BeforeAll: This annotation denotes that the annotated method will be executed before all test methods in the current class.
-//@AfterAll: This annotation denotes that the annotated method will be executed after all test methods in the current class.
+import model.Algorithm.LogGenerator;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
-// This is main.
 
 public class AlgorithmsTest {
-
-
+    @Test
+    public void test(){
+        assertEquals(1,1);
+    }
+//jinabihuow caoninid lien snieghur nuenhgesj lajineghe j
 }
