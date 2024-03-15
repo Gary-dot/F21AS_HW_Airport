fa21ab6ee6145286b80dcf59927e47038f507ee4
Jiuzhou Zhang <jz2018@hw.ac.uk>
2024-03-15 20:51:53 +0000

    reset version

--- src/model/DataStructure/FlightList.java
+++ src/model/DataStructure/FlightList.java
@@ -1,6 +1,5 @@
 package model.DataStructure;
 
-
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -20,14 +19,29 @@ public class FlightList {
         arrayList = new ArrayList<Flight>();
     }
 
+    /**
+     * Perform any initialization for the address book.
+     * Only used for testing purposes.
+     * @param path
+     */
+    public FlightList(String path) {
+        hashMap = new HashMap<String, Flight>();
+        arrayList = new ArrayList<Flight>();
+        readFlightList(this, path);
+    }
+
     public ArrayList<Flight> getArrayList() {
         return arrayList;
     }
 
+    /**
+     * Singleton pattern
+     * @return
+     */
     public static synchronized FlightList getInstance() {
         if (instance == null) {
             instance = new FlightList();
-            readFlightList(instance);
+            readFlightList(instance, ".\\input\\FlightList.txt");
         }
         return instance;
     }
