41c7eac58906102b70260a7b513ebd9c46a744b3
Jiuzhou Zhang <jz2018@hw.ac.uk>
2024-03-15 22:13:54 +0000

    Merge

--- src/JUnitTest/DatastructureTest/BaggageTest.java
+++ src/JUnitTest/DatastructureTest/BaggageTest.java
@@ -1,25 +1,21 @@
-package JUnitTest;
+package JUnitTest.DatastructureTest;
 
-import DataStructure.Baggage;
-import DataStructure.Exceptions.WrongBaggageSizeFormatException;
-import org.junit.jupiter.api.Test;
+//JUnit 4
+//import org.junit.Test;
+//import static org.junit.Assert.assertEquals;
 
-<<<<<<< HEAD
-import static org.junit.jupiter.api.Assertions.assertEquals;
-=======
 //JUnit 5, including
 //@Test: This annotation denotes that a method is a test method.
 //@BeforeEach: This annotation denotes that the annotated method will be executed before each test method.
 //@AfterEach: This annotation denotes that the annotated method will be executed after each test method.
 //@BeforeAll: This annotation denotes that the annotated method will be executed before all test methods in the current class.
 //@AfterAll: This annotation denotes that the annotated method will be executed after all test methods in the current class.
-import DataStructure.Baggage;
+import model.DataStructure.*;
+import model.DataStructure.Exceptions.*;
 import org.junit.jupiter.api.*;
-
 import static org.junit.jupiter.api.Assertions.*;
->>>>>>> 7a25851 (Optimize the function for reading files so that the program won't stop even if it reads a line of invalid information.)
-// This is main.
-public class DataStructureTest {
+
+public class BaggageTest {
     /**
      * Test the Baggage class and WrongBaggageSizeFormatException class.
      */
@@ -31,7 +27,6 @@ public class DataStructureTest {
         assertEquals(9, baggage.getHeight());
         assertEquals(50, baggage.getWeight());
         assertEquals(2772, baggage.getVolume());
-
         try {
             new Baggage(27, 10, 14, 60); //length > width, but width < height
         } catch (WrongBaggageSizeFormatException e) {
